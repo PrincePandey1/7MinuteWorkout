17c17c19d98e4496cbe9fd7d5441890ad24b697e
Prince Pandey <princekupandey786@gmail.com>
2021-03-08 12:41:34 +0530

    Added New Resource File in drawable

--- app/src/main/java/com/example/android/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/android/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener {
+            Toast.makeText(this,
+                "Here we will start the Exercise" ,
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
