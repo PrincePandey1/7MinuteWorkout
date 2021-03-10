fa5aff2876e073e904048aa8f204f7377e2b74ea
Prince Pandey <princekupandey786@gmail.com>
2021-03-10 23:48:03 +0530

    Added Toolbar

--- app/src/main/java/com/example/android/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/android/a7minuteworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.a7minuteworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,9 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         llStart.setOnClickListener {
-            Toast.makeText(this,
-                "Here we will start the Exercise" ,
-                Toast.LENGTH_LONG).show()
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
 }
